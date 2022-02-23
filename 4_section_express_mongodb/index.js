6555a5ae2c2649dab7224c61c6b80ed4dc5764b4
a.kazakov <a.kazakov@bidzaar.com>
2022-02-23 22:47:59 +0300

    Connect mongodb

--- 4_section_express_mongodb/index.js
+++ 4_section_express_mongodb/index.js
@@ -7,6 +7,8 @@ const homeRoutes = require("./routes/home");
 const cardRoutes = require("./routes/card");
 const coursesRoutes = require("./routes/courses");
 
+const mongoose = require("mongoose");
+
 
 const app = express();
 const hbs = exphds.create({
@@ -26,9 +28,21 @@ app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/card', cardRoutes);
 
+async function start() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://kazak_admin_helol:ISnfNsRJElNkAtb6@cluster0.tjxko.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      { useNewUrlParser: true }
+    );
+  
+    /** Server */
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-/** Server */
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+start();
