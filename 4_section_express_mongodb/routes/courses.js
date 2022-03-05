ba890c8d080ff52c3a76b8744e1211f6991a1a4d
a.kazakov <a.kazakov@bidzaar.com>
2022-03-05 12:23:55 +0300

    Add delete course

--- 4_section_express_mongodb/routes/courses.js
+++ 4_section_express_mongodb/routes/courses.js
@@ -32,11 +32,19 @@ router.get("/:id/edit", async (req, res) => {
 });
 
 router.post("/edit", async (req, res) => {
-  console.log("=>", req.body);
   const {id} = req.body;
   await Course.findByIdAndUpdate(id, req.body).lean();
 
   res.redirect("/courses");
+});
+
+router.post("/delete", async (req, res) => {
+  try {
+    await Course.deleteOne({ _id: req.body.id });
+    res.redirect("/courses")
+  } catch (e) {
+    console.log(e)
+  }
 });
 
 module.exports = router;
